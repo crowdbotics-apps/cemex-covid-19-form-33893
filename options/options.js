b2b448ebac4dc21fdfd8946638be715e8c984c40
Crowdbotics <team@crowdbotics.com>
2022-02-28 13:59:21 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "cemex_covid_19_form_33893",
+  url: "https://cemex_covid_19_form_33893.botics.co",
+  api: "https://cemex_covid_19_form_33893.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
